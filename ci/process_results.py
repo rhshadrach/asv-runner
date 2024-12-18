086e7f434630b6063a2df3dfc2d172d1c5f7d459
Richard Shadrach <rhshadrach@gmail.com>
2024-12-18 16:33:09 -0500

    fixp

--- ci/process_results.py
+++ ci/process_results.py
@@ -1,18 +1,14 @@
 import json
-import subprocess
-from pathlib import Path
 import datetime as dt
 import pyarrow as pa
 import itertools as it
 import pandas as pd
-
-# asv machine --machine=asvrunner --yes
-# asv run --durations=30 --python=same --set-commit-hash=$(git rev-parse HEAD) --show-stderr --machine=asvrunner -b ^groupby.GroupByCythonAgg
-
+import os
 
 input_filename = "results.json"
 output_filename = "results.parquet"
 results = json.load(open(input_filename))
+commit_hash = results["commit_hash"]
 columns = results["result_columns"]
 buf = {"name": [], "params": [], "result": []}
 for name, benchmark in results['results'].items():
@@ -27,11 +23,12 @@ buf["params"] = pd.array(buf["params"], dtype=pd.ArrowDtype(pa.list_(pa.string()
 buf["result"] = pd.array(buf["result"], dtype="float64[pyarrow]")
 df = pd.DataFrame(buf)
 df["date"] = pd.array([dt.datetime.today()] * len(df), dtype=pd.ArrowDtype(pa.timestamp("us")))
-df["sha"] = pd.array([githash] * len(df), dtype="string[pyarrow]")
+df["sha"] = pd.array([commit_hash] * len(df), dtype="string[pyarrow]")
 df = df[["date", "sha", "name", "params", "result"]]
 
-# # TODO: Read from current branch
-# existing = pd.read_parquet("results.parquet")
-# final = pd.concat([existing, df])
-final = df
-final.to_parquet(output_filename)
+if os.path.exists("data/results.parquet"):
+    existing = pd.read_parquet("results.parquet")
+    final = pd.concat([existing, df])
+else:
+    final = df
+final.to_parquet(f"data/{output_filename}")
