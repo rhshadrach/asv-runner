6627e36abbf825265175cb7ffad4053b124cc599
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2024-12-21 14:34:22 -0500

    Avoid race condition (#6)

--- ci/process_results.py
+++ ci/process_results.py
@@ -4,31 +4,42 @@ import pyarrow as pa
 import itertools as it
 import pandas as pd
 import os
+import argparse
+from pathlib import Path
 
-input_filename = "results.json"
-output_filename = "results.parquet"
-results = json.load(open(input_filename))
-commit_hash = results["commit_hash"]
-columns = results["result_columns"]
-buf = {"name": [], "params": [], "result": []}
-for name, benchmark in results['results'].items():
-    data = dict(zip(columns, benchmark))
-    result = data["result"]
-    params = [", ".join(e) for e in it.product(*data["params"])]
-    buf["name"].extend([name] * len(result))
-    buf["params"].extend(params)
-    buf["result"].extend(result)
-buf["name"] = pd.array(buf["name"], dtype="string[pyarrow]")
-buf["params"] = pd.array(buf["params"], dtype="string[pyarrow]")
-buf["result"] = pd.array(buf["result"], dtype="float64[pyarrow]")
-df = pd.DataFrame(buf)
-df["date"] = pd.array([dt.datetime.today()] * len(df), dtype=pd.ArrowDtype(pa.timestamp("us")))
-df["sha"] = pd.array([commit_hash] * len(df), dtype="string[pyarrow]")
-df = df[["date", "sha", "name", "params", "result"]]
+def run(input_path: str, output_path: str | Path):
+    if not isinstance(output_path, Path):
+        output_path = Path(output_path)
+    results = json.load(open(input_path))
+    commit_hash = results["commit_hash"]
+    columns = results["result_columns"]
+    buf = {"name": [], "params": [], "result": []}
+    for name, benchmark in results['results'].items():
+        data = dict(zip(columns, benchmark))
+        result = data["result"]
+        params = [", ".join(e) for e in it.product(*data["params"])]
+        buf["name"].extend([name] * len(result))
+        buf["params"].extend(params)
+        buf["result"].extend(result)
+    buf["name"] = pd.array(buf["name"], dtype="string[pyarrow]")
+    buf["params"] = pd.array(buf["params"], dtype="string[pyarrow]")
+    buf["result"] = pd.array(buf["result"], dtype="float64[pyarrow]")
+    df = pd.DataFrame(buf)
+    df["date"] = pd.array([dt.datetime.today()] * len(df), dtype=pd.ArrowDtype(pa.timestamp("us")))
+    df["sha"] = pd.array([commit_hash] * len(df), dtype="string[pyarrow]")
+    df = df[["date", "sha", "name", "params", "result"]]
 
-if os.path.exists("data/results.parquet"):
-    existing = pd.read_parquet("data/results.parquet")
-    final = pd.concat([existing, df])
-else:
-    final = df
-final.to_parquet(f"data/{output_filename}")
+    parquet_path = output_path / "results.parquet"
+    if os.path.exists(parquet_path):
+        existing = pd.read_parquet(parquet_path)
+        final = pd.concat([existing, df])
+    else:
+        final = df
+    final.to_parquet(parquet_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path")
+    parser.add_argument("--output-path")
+    args = parser.parse_args()
+    run(input_path=args.input_path, output_path=args.output_path)
